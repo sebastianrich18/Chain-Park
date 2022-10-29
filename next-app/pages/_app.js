3dad79bc9aa13f48853b5b623b4542f48baf60c0
Sebastian <sebastianrichel18@gmail.com>
2022-10-28 20:07:47 -0400

    now using web3modal v2

--- next-app/pages/_app.js
+++ next-app/pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
+import { Web3Modal } from '@web3modal/react'
+import { chains, providers } from '@web3modal/ethereum'
 
+const config = {
+  projectId: '21951b1cdb76de6c32fb1283cd60d8f8',
+  theme: 'dark',
+  accentColor: 'default',
+  ethereum: {
+    appName: 'ChainPark',
+    chains: [chains.goerli, chains.sepolia],
+    providers: [providers.walletConnectProvider({projectId: '21951b1cdb76de6c32fb1283cd60d8f8'})]
+  },
+  autoConnect: false
+}
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Web3Modal config={config} />
+    </>
+  )
 }
 
 export default MyApp
--- next-app/pages/index.js
+++ next-app/pages/index.js
@@ -1,118 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Web3Modal from "web3modal";
-import { providers, Contract } from "ethers";
-import { useEffect, useRef, useState } from "react";
+import { useAccount, Web3Button } from "@web3modal/react"
+
 
 
 export default function Home() {
-  const CHAIN_ID = 5; // 5 for Goerli
-
-  // walletConnected keep track of whether the user's wallet is connected or not
-  const [walletConnected, setWalletConnected] = useState(false);
-
-  // Create a reference to the Web3 Modal (used for connecting to Metamask) which persists as long as the page is open
-  const web3ModalRef = useRef();
-
-  // loading is set to true when we are waiting for a transaction to get mined
-  const [loading, setLoading] = useState(false);
-
-  /**
- * Returns a Provider or Signer object representing the Ethereum RPC with or without the
- * signing capabilities of metamask attached
- *
- * A `Provider` is needed to interact with the blockchain - reading transactions, reading balances, reading state, etc.
- *
- * A `Signer` is a special type of Provider used in case a `write` transaction needs to be made to the blockchain, which involves the connected account
- * needing to make a digital signature to authorize the transaction being sent. Metamask exposes a Signer API to allow your website to
- * request signatures from the user using Signer functions.
- *
- * @param {*} needSigner - True if you need the signer, default false otherwise
- */
-  const getProviderOrSigner = async (needSigner = false) => {
-    // Connect to Metamask
-    // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
-    const provider = await web3ModalRef.current.connect();
-    const web3Provider = new providers.Web3Provider(provider);
-
-    // If user is not connected to the Rinkeby network, let them know and throw an error
-    const { chainId } = await web3Provider.getNetwork();
-    if (chainId !== CHAIN_ID) {
-      window.alert("Change the network to id: " + CHAIN_ID);
-      throw new Error("Change network to id: " + CHAIN_ID);
-    }
-
-    if (needSigner) {
-      const signer = web3Provider.getSigner();
-      return signer;
-    }
-    return web3Provider;
-  };
-
-  const connectWallet = async () => {
-    try {
-      // Get the provider from web3Modal, which in our case is MetaMask
-      // When used for the first time, it prompts the user to connect their wallet
-      await getProviderOrSigner();
-      setWalletConnected(true);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  const renderButton = () => {
-    if (walletConnected) {
-      if (loading) {
-        return <button className={styles.button}>Loading...</button>;
-      } else {
-        return (
-          <button className={styles.button}>
-            Wallet Connected
-          </button>
-        );
-      }
-    } else {
-      return (
-        <button onClick={connectWallet} className={styles.button}>
-          Connect your wallet
-        </button>
-      );
-    }
-  };
-
-  // useEffects are used to react to changes in state of the website
-  // The array at the end of function call represents what state changes will trigger this effect
-  // In this case, whenever the value of `walletConnected` changes - this effect will be called
-  useEffect(() => {
-    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
-    if (!walletConnected) {
-      // Assign the Web3Modal class to the reference object by setting it's `current` value
-      // The `current` value is persisted throughout as long as this page is open
-      web3ModalRef.current = new Web3Modal({
-        network: "goerli",
-        providerOptions: {},
-        disableInjectedProvider: false,
-      });
-      connectWallet();
-    }
-  }, [walletConnected]);
+  const { account } = useAccount()
 
   return (
     <div>
-      <Head>
-        <title>Chain Park</title>
-        <meta name="description" content="Whitelist-Dapp" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className={styles.main}>
-        <div>
-          <h1 className={styles.title}>Welcome to UB Chain Park!</h1>
-          <div className={styles.description}>
-            {renderButton()}
-          </div>
-        </div>
-      </div>
+      {account.isConnected ? <h1>Connected to {account.address}</h1> : null}
+      <Web3Button />
     </div>
   )
 }
